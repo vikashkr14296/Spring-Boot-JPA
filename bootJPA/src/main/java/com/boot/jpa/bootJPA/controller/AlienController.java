be13f81a936e61f30176778ac1f8093c4de18cef
vikas <vikas@VIKU-PC>
2024-01-09 11:08:00 +0530

    Return the data in Json Format

--- bootJPA/src/main/java/com/boot/jpa/bootJPA/controller/AlienController.java
+++ bootJPA/src/main/java/com/boot/jpa/bootJPA/controller/AlienController.java
@@ -1,5 +1,8 @@
 package com.boot.jpa.bootJPA.controller;
 
+import java.util.List;
+import java.util.Optional;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -32,19 +35,19 @@ public class AlienController {
 	
 	@RequestMapping("/aliens")
 	@ResponseBody
-	public String getAliens()
+	public List<Alien> getAliens()
 	{
 		
-		return repo.findAll().toString();
+		return repo.findAll();
 		
 	}
 	
 	@RequestMapping("/alien/{aid}")
 	@ResponseBody
-	public String getAlien(@PathVariable("aid") int aid)
+	public Optional<Alien> getAlien(@PathVariable("aid") int aid)
 	{
 		
-		return repo.findById(aid).toString();
+		return repo.findById(aid);
 		
 	}
 	
--- bootJPA/src/main/java/com/boot/jpa/bootJPA/dao/AlienRepo.java
+++ bootJPA/src/main/java/com/boot/jpa/bootJPA/dao/AlienRepo.java
@@ -2,12 +2,13 @@ package com.boot.jpa.bootJPA.dao;
 
 import java.util.List;
 
+import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
 
 import com.boot.jpa.bootJPA.model.Alien;
 
-public interface AlienRepo extends CrudRepository<Alien, Integer>
+public interface AlienRepo extends JpaRepository<Alien, Integer>
 {
 	
 	
