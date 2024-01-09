a23ca70b92b123b6dd0adc7ba5db400b3cad36b0
vikas <vikas@VIKU-PC>
2024-01-09 12:36:57 +0530

    Had created mapping for delete and put.

--- bootJPA/src/main/java/com/boot/jpa/bootJPA/controller/AlienController.java
+++ bootJPA/src/main/java/com/boot/jpa/bootJPA/controller/AlienController.java
@@ -5,9 +5,11 @@ import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -30,6 +32,16 @@ public class AlienController {
 		return "home.jsp";
 	}
 	
+	@DeleteMapping("/alien/{aid}")
+	public String deleteAlien(@PathVariable int aid)
+	{
+		Alien a = repo.getOne(aid);
+		
+		repo.delete(a);
+		
+		return "Deleted";
+	}
+	
 	@PostMapping(path="/alien", consumes={"application/json"})
 	public Alien addAlien(@RequestBody Alien alien)
 	{
@@ -43,6 +55,13 @@ public class AlienController {
 		
 		return repo.findAll();
 		
+	}
+	
+	@PutMapping(path="/alien", consumes={"application/json"})
+	public Alien saveOrUpdateAlien(@RequestBody Alien alien)
+	{
+		repo.save(alien);
+		return alien;
 	}
 	
 	@RequestMapping("/alien/{aid}")
